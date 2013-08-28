63d42088c50cf1338aba388d907ac665d48bf79e
Aaron <aaron.l.france@gmail.com>
2013-08-28 23:10:47 +0200

    Initial commit - Endpoint / is working and typed correctly.

---
+++ droplets.go
@@ -0,0 +1,63 @@
+package digitalocean
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var (
+	Endpoint string = "https://api.digitalocean.com/droplets/%s?client_id=%s&api_key=%s"
+)
+
+func Settings(ClientID, APIKey string) {
+	Endpoint = fmt.Sprintf(Endpoint, "%s", ClientID, APIKey)
+	fmt.Println(Endpoint)
+}
+
+type Droplet struct {
+	ID             int64
+	Name           string
+	Image_ID       int
+	Size_ID        int
+	Region_ID      int
+	Backups_Active bool
+	IP_Address     string
+	Locked         bool
+	Status         string
+	Created_At     time.Time
+}
+
+func baserequest(URL string, Type interface{}) (interface{}, error) {
+	resp, err := http.Get(fmt.Sprintf(Endpoint, ""))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(b, Type)
+	if err != nil {
+		return nil, err
+	}
+	return Type, nil
+}
+
+func Droplets() ([]Droplet, error) {
+	type Request struct {
+		Status   string
+		Droplets []Droplet
+	}
+	resp, err := baserequest(fmt.Sprintf(Endpoint, ""), &Request{})
+	if err != nil {
+		return nil, err
+	}
+	if val, ok := resp.(*Request); ok {
+		return val.Droplets, nil
+	}
+	return nil, err
+}
