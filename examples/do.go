82b3e8b843e25985a3da461c7f4cca2299dad0f4
AeroNotix <aaron.l.france@gmail.com>
2013-08-29 09:56:30 +0100

    Added more the to example to show all the current API functions.

--- examples/do.go
+++ examples/do.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/AeroNotix/digitalocean"
+	"strings"
 )
 
 func main() {
-	digitalocean.Settings("ClientCode", "APIKey")
-	fmt.Println(digitalocean.Droplets())
+	digitalocean.Settings("ClientID", "APIKey")
+	droplets, err := digitalocean.Droplets()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(strings.Repeat("-", 50))
+	for _, droplet := range droplets {
+		fmt.Println(digitalocean.DropletByID(droplet.ID))
+	}
 }
